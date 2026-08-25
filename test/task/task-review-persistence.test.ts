1077f0900364f18d00937df7b681acc47bb55e41
Vistyy <kaszuraszymon@gmail.com>
2026-08-25 22:34:13 +0200

    Apply Task Review test import ordering

--- test/task/task-review-persistence.test.ts
+++ test/task/task-review-persistence.test.ts
@@ -19,8 +19,8 @@ import {
   taskReviewAdmissionRejection,
 } from "../../src/task/adapters/sqlite/sqliteTaskReviewPersistence.js";
 import { withTaskReviewRecoveryUseCases } from "../../src/task/composition/loadTaskReviewUseCases.js";
-import type { TaskSimplificationAdvice } from "../../src/task/review/taskSimplificationAdvice.js";
 import { expectedTaskReviewWorkspacePath } from "../../src/task/review/taskReviewWorkspace.js";
+import type { TaskSimplificationAdvice } from "../../src/task/review/taskSimplificationAdvice.js";
 import { publicTaskId } from "../../src/task/taskId.js";
 import { openSqliteTaskChangeReviewAdmissionPersistence } from "../../src/taskChange/adapters/sqlite/sqliteTaskChangeReviewAdmissionPersistence.js";
 import { createGitRepo } from "../support/by-cli.js";
